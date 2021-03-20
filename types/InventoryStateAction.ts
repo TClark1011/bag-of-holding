1ae844a6cdd6c7d9260c2d53b5e7e80a52c9e345
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-20 16:24:48 +1100

    types for actions upon inventory state

---
+++ types/InventoryStateAction.ts
@@ -0,0 +1,21 @@
+import InventoryItemFields from "./InventoryItemFields";
+
+export type InventoryStateActionType = "item_add";
+
+/**
+ * Valid types that can be passed into the 'data' field of
+ * 'InventoryStateAction' typed objects
+ *
+ * Valid Types are:
+ * - InventoryItemFields - When creating/updating an item, the data should
+ * be the all the data for that item.
+ * - string - When deleting an item, the '_id' of the item to delete
+ */
+export type InventoryStateActionValidData = InventoryItemFields | string;
+
+interface InventoryStateAction {
+	type: InventoryStateActionType;
+	data: InventoryStateActionValidData;
+}
+
+export default InventoryStateAction;
