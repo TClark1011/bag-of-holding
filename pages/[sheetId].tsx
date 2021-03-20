62c9fdc4d9238f0353395a3551cf80f60e4f7e1a
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-20 11:35:03 +1100

    created page for sheets

--- pages/[sheetId].tsx
+++ pages/[sheetId].tsx
@@ -1,12 +1,17 @@
 import { Box, Heading } from "@chakra-ui/layout";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
+import InventorySheetFields from "../types/InventorySheetFields";
 
 /**
  * The page for a specific sheet
  *
+ * @param {object} props The component props
+ * @param {string} props.name The name of the sheet
+ * @param {string[]} props.items The items in the sheet
  * @returns {React.ReactElement} Sheet component
  */
-const Sheet: React.FC = () => {
+const Sheet: React.FC<InventorySheetFields> = ({ name, items }) => {
 	return (
 		<Box>
 			<Head>
@@ -14,10 +19,39 @@ const Sheet: React.FC = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
-				<Heading>Sheet!</Heading>
+				<Heading>{name}</Heading>
 			</main>
 		</Box>
 	);
+};
+
+/**
+ * Get the static props
+ *
+ * @returns {GetStaticPropsResult<InventorySheetFields>} The props for the sheet
+ */
+export const getStaticProps: GetStaticProps<InventorySheetFields> = async () => ({
+	props: {
+		name: "Test Sheet",
+		items: [],
+	},
+});
+
+/**
+ * Determine static paths to pre render
+ *
+ * @returns {GetStaticPathsResult} Data for pre rendering paths
+ */
+export const getStaticPaths: GetStaticPaths = async () => {
+	const paths = [
+		{
+			params: { sheetId: "1" },
+		},
+	]; //This will replaced with a map function that returns an array with all the sheets that exist in the database
+	return {
+		paths,
+		fallback: true,
+	};
 };
 
 export default Sheet;
