8efbc7f363594416a440aa201ecf520826cdb0cb
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-20 11:48:06 +1100

    added initial data display

--- pages/[sheetId].tsx
+++ pages/[sheetId].tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from "@chakra-ui/layout";
+import { Box, Heading, List, ListItem, Text } from "@chakra-ui/layout";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import InventorySheetFields from "../types/InventorySheetFields";
@@ -9,9 +9,10 @@ import InventorySheetFields from "../types/InventorySheetFields";
  * @param {object} props The component props
  * @param {string} props.name The name of the sheet
  * @param {string[]} props.items The items in the sheet
+ * @param props.members
  * @returns {React.ReactElement} Sheet component
  */
-const Sheet: React.FC<InventorySheetFields> = ({ name, items }) => {
+const Sheet: React.FC<InventorySheetFields> = ({ name, items, members }) => {
 	return (
 		<Box>
 			<Head>
@@ -20,6 +21,20 @@ const Sheet: React.FC<InventorySheetFields> = ({ name, items }) => {
 			</Head>
 			<main>
 				<Heading>{name}</Heading>
+				<Text fontWeight="bold">PartyMembers:</Text>
+				<List>
+					{members.map((item) => (
+						<ListItem key={item}>{item}</ListItem>
+					))}
+				</List>
+				<Text fontWeight="bold">Items:</Text>
+				<List>
+					{items.length ? (
+						items.map((item) => <ListItem key={item._id}>{item.name}</ListItem>)
+					) : (
+						<ListItem>The inventory is currently empty</ListItem>
+					)}
+				</List>
 			</main>
 		</Box>
 	);
@@ -34,6 +49,7 @@ export const getStaticProps: GetStaticProps<InventorySheetFields> = async () => 
 	props: {
 		name: "Test Sheet",
 		items: [],
+		members: ["Vincent", "Archie", "Sen", "Seath"],
 	},
 });
 
