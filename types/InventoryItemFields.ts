7882467d1505c8929443295321eb60cd353053c9
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-20 12:06:06 +1100

    extra field and jsdocs to 'InventoryItemFields'

--- types/InventoryItemFields.ts
+++ types/InventoryItemFields.ts
@@ -1,3 +1,15 @@
+/**
+ * @typedef {object} InventoryItemFields The fields for an inventory item
+ * @property {string} _id The unique identifier
+ * @property {string} name The name of the item
+ * @property {number} quantity The quantity of the item
+ * @property {string} [type] The type (essentially a category) of the item
+ * @property {string} [description] Description of the item
+ * @property {string} [carriedBy] The party member that is carrying the item
+ * @property {number} [weight] How much the item weights (in 'lb')
+ * @property {number} [value] The monetary value of the item
+ * @property {string} [infoLink] A link to a document that contains more information about the item
+ */
 interface InventoryItemFields {
 	_id: string;
 	name: string;
@@ -7,6 +19,7 @@ interface InventoryItemFields {
 	carriedBy?: string;
 	weight?: number;
 	value?: number;
+	infoLink?: string;
 }
 
 export default InventoryItemFields;
