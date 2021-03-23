e770bd598a101b5ec5c303e8d56db1b7f35089dd
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-23 12:31:27 +1100

    added jsdocs for 'TableCell'

--- components/ui/TableCell.tsx
+++ components/ui/TableCell.tsx
@@ -1,7 +1,10 @@
 import { TableCellProps, Td } from "@chakra-ui/table";
 
 /**
- * @param props
+ * A component to be used as a table cell
+ *
+ * @param {chakra.TableCellProps} props TableCell props
+ * @returns {React.ReactElement} Rendered stuff
  */
 const TableCell: React.FC<TableCellProps> = (props) => (
 	<Td paddingX={2} textAlign="center" {...props} />
