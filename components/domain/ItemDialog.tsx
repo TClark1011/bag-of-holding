20a2f561ecab2e809f635b2aa92c91e073e6b528
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-28 19:01:32 +1100

    added a fixmecomment

--- components/domain/ItemDialog.tsx
+++ components/domain/ItemDialog.tsx
@@ -35,6 +35,7 @@ interface Props {
  * @returns {React.ReactElement} The rendered HTML
  */
 const ItemDialog: React.FC<Props> = ({ mode }) => {
+	//FIXME: Trying to open the edit interface after just having deleted an item crashes the app
 	const inEditMode = mode === "edit";
 
 	const { activeItem, closeDialog } = useSheetPageState();
