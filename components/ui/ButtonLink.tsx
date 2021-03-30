9c167fe1661487c074cdcfecd4f7c1ae9fac7eb2
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-31 09:50:50 +1100

    'ButtonLink' now uses 'FlexibleLink'

--- components/ui/ButtonLink.tsx
+++ components/ui/ButtonLink.tsx
@@ -1,21 +1,33 @@
 import { Button, ButtonProps } from "@chakra-ui/button";
-import Link from "next/link";
-
-export interface ButtonLinkProps extends ButtonProps {
-	href: string;
-}
+import FlexibleLink, { FlexibleLinkProps } from "./FlexibleLink";
 
 /**
  * A button that acts as a link
  *
  * @param {object} props The props
  * @param {string} props.href The link destination
+ * @param {boolean} [props.useNextLink] see 'FlexibleLinkProps'
+ * @param {object} [props.nextLinkProps] see 'FlexibleLinkProps'
+ * @param {object} [props.chakraLinkProps] see 'FlexibleLinkProps'
  * @returns {React.ReactElement} Component stuff
  */
-const ButtonLink: React.FC<ButtonLinkProps> = ({ href, ...props }) => (
-	<Link href={href}>
-		<Button {...props} />
-	</Link>
-);
+const ButtonLink: React.FC<ButtonProps & FlexibleLinkProps> = ({
+	href,
+	useNextLink,
+	nextLinkProps,
+	chakraLinkProps,
+	...props
+}) => {
+	return (
+		<FlexibleLink
+			href={href}
+			useNextLink={useNextLink}
+			nextLinkProps={nextLinkProps}
+			chakraLinkProps={chakraLinkProps}
+		>
+			<Button {...props} />
+		</FlexibleLink>
+	);
+};
 
 export default ButtonLink;
