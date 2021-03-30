0b6ab687487b7e546da992cf0cd9754e9f290d00
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-30 11:31:10 +1100

    added color mode switch to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@chakra-ui/button";
-import { Box, Center, Heading, VStack } from "@chakra-ui/layout";
+import { Box, Center, Flex, Heading, VStack } from "@chakra-ui/layout";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { appDisplayTitle, appDomain } from "../constants/branding";
@@ -7,6 +7,7 @@ import WelcomeBack from "../components/domain/Home/WelcomeBack";
 import BagOfHoldingIcon from "../components/icons/BagOfHoldingIcon";
 import { use100vh } from "react-div-100vh";
 import Meta from "../components/templates/Meta";
+import ColorModeSwitch from "../components/domain/ColorModeSwitch";
 
 /**
  * Home component
@@ -36,32 +37,37 @@ const Home: React.FC = () => {
 		<>
 			<Meta url={appDomain} />
 			<main>
-				<Center width="full" minHeight={screenHeight} padding="break">
-					<Box>
-						<VStack spacing="break" marginBottom="break">
-							<Center>
-								<BagOfHoldingIcon fill="white" boxSize={[40, 44, 52]} />
-							</Center>
-							{/* Main Title */}
-							<Heading textAlign="center" textStyle="h1" as="h1" width="full">
-								{appDisplayTitle}
-							</Heading>
-							<Heading textAlign="center" textStyle="h2" as="h2" width="full">
-								Track your party{"'"}s inventory, no matter what you play
-							</Heading>
-							<Center>
-								<Button
-									onClick={getNewSheet}
-									isLoading={newSheetIsLoading}
-									colorScheme="primary"
-								>
-									Get Started
-								</Button>
-							</Center>
-						</VStack>
-						<WelcomeBack />
-					</Box>
-				</Center>
+				<Flex direction="column">
+					<Flex padding={2} width="full" justify="flex-end">
+						<ColorModeSwitch variant="ghost" />
+					</Flex>
+					<Center width="full" flexGrow={1} padding="break">
+						<Box>
+							<VStack spacing="break" marginBottom="break">
+								<Center>
+									<BagOfHoldingIcon fill="white" boxSize={[40, 44, 52]} />
+								</Center>
+								{/* Main Title */}
+								<Heading textAlign="center" textStyle="h1" as="h1" width="full">
+									{appDisplayTitle}
+								</Heading>
+								<Heading textAlign="center" textStyle="h2" as="h2" width="full">
+									Track your party{"'"}s inventory, no matter what you play
+								</Heading>
+								<Center>
+									<Button
+										onClick={getNewSheet}
+										isLoading={newSheetIsLoading}
+										colorScheme="primary"
+									>
+										Get Started
+									</Button>
+								</Center>
+							</VStack>
+							<WelcomeBack />
+						</Box>
+					</Center>
+				</Flex>
 			</main>
 		</>
 	);
