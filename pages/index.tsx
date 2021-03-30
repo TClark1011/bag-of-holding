cd116fa974225a87ae1a80c924b3a0845f6ead66
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-31 09:25:29 +1100

    changed homepage content layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,60 +43,62 @@ const Home: React.FC = () => {
 		<>
 			<Meta url={appDomain} />
 			<main>
-				{/* Content Wrapper */}
-				<Flex direction="column" minHeight={screenHeight}>
-					{/* Top Bar */}
-					<Flex justify="space-between" padding={2} width="full">
-						<ColorModeSwitch />
-						<HStack justify="flex-end">
-							<IconLink
-								href={infoPageUrl}
-								aria-label="link to info page"
-								icon={<HelpOutlineIcon boxSize="icon" />}
-								variant="ghost"
-							/>
-							<IconLink
-								href={contactPageUrl}
-								aria-label="link to contact page"
-								icon={<MailOutlineIcon boxSize="icon" />}
-								variant="ghost"
-							/>
-							<GitLink />
-						</HStack>
-					</Flex>
-					{/* Main Content */}
-					<Center width="full" flexGrow={1} padding="break">
-						<Box>
-							<VStack spacing="break" marginBottom="break">
-								<Center>
-									<BagOfHoldingIcon fill="white" boxSize={[40, 44, 52]} />
-								</Center>
-								{/* Main Title */}
-								<Heading textAlign="center" textStyle="h1" as="h1" width="full">
-									{appDisplayTitle}
-								</Heading>
-								<Heading textAlign="center" textStyle="h2" as="h2" width="full">
-									Track your party{"'"}s inventory, no matter what you play
-								</Heading>
-								<Center>
-									<VStack spacing="break">
-										<Button
-											onClick={getNewSheet}
-											isLoading={newSheetIsLoading}
-											colorScheme="primary"
-										>
-											Get Started
-										</Button>
-										<ButtonLink href={infoPageUrl} variant="ghost" size="xs">
-											What is this?
-										</ButtonLink>
-									</VStack>
-								</Center>
-							</VStack>
-							<WelcomeBack />
-						</Box>
-					</Center>
+				{/* Top Bar */}
+				<Flex
+					justify="space-between"
+					padding={2}
+					width="full"
+					position="absolute"
+				>
+					<ColorModeSwitch />
+					<HStack justify="flex-end">
+						<IconLink
+							href={infoPageUrl}
+							aria-label="link to info page"
+							icon={<HelpOutlineIcon boxSize="icon" />}
+							variant="ghost"
+						/>
+						<IconLink
+							href={contactPageUrl}
+							aria-label="link to contact page"
+							icon={<MailOutlineIcon boxSize="icon" />}
+							variant="ghost"
+						/>
+						<GitLink />
+					</HStack>
 				</Flex>
+				{/* Main Content */}
+				<Center width="full" minHeight={screenHeight} padding="break">
+					<Box>
+						<VStack spacing="break" marginBottom="break">
+							<Center>
+								<BagOfHoldingIcon fill="white" boxSize={[40, 44, 52]} />
+							</Center>
+							{/* Main Title */}
+							<Heading textAlign="center" textStyle="h1" as="h1" width="full">
+								{appDisplayTitle}
+							</Heading>
+							<Heading textAlign="center" textStyle="h2" as="h2" width="full">
+								Track your party{"'"}s inventory, no matter what you play
+							</Heading>
+							<Center>
+								<VStack spacing="break">
+									<Button
+										onClick={getNewSheet}
+										isLoading={newSheetIsLoading}
+										colorScheme="primary"
+									>
+										Get Started
+									</Button>
+									<ButtonLink href={infoPageUrl} variant="ghost" size="xs">
+										What is this?
+									</ButtonLink>
+								</VStack>
+							</Center>
+						</VStack>
+						<WelcomeBack />
+					</Box>
+				</Center>
 			</main>
 		</>
 	);
