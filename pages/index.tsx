80bf87586348b91e29aa64778364089ce8a5b71e
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-31 10:04:45 +1100

    get started button now links to 'new' page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@chakra-ui/button";
-import { Box, Center, Flex, Heading, HStack, VStack } from "@chakra-ui/layout";
-import { useState } from "react";
-import { useRouter } from "next/router";
+import {
+	Box,
+	Center,
+	Flex,
+	Heading,
+	HStack,
+	LinkBox,
+	VStack,
+} from "@chakra-ui/layout";
 import { appDisplayTitle, appDomain } from "../constants/meta";
 import WelcomeBack from "../components/domain/Home/WelcomeBack";
 import BagOfHoldingIcon from "../components/icons/BagOfHoldingIcon";
@@ -11,7 +17,6 @@ import ColorModeSwitch from "../components/ui/ColorModeSwitch";
 import GitLink from "../components/ui/GitLink";
 import { HelpOutlineIcon, MailOutlineIcon } from "chakra-ui-ionicons";
 import IconLink from "../components/ui/IconLink";
-import getSheetLink from "../utils/getSheetLink";
 import ButtonLink from "../components/ui/ButtonLink";
 import { contactPageUrl, infoPageUrl } from "../constants/urls";
 
@@ -21,22 +26,6 @@ import { contactPageUrl, infoPageUrl } from "../constants/urls";
  * @returns {React.ReactElement} The home page
  */
 const Home: React.FC = () => {
-	const [newSheetIsLoading, setNewSheetIsLoading] = useState<boolean>(false);
-	const router = useRouter();
-	/**
-	 * Create a new sheet, then redirect the user to that sheet
-	 */
-	const getNewSheet = () => {
-		setNewSheetIsLoading(true);
-		fetch("api/sheet", { method: "POST" })
-			.then((res) => res.json())
-			.then((data) => {
-				router.push(getSheetLink(data as string));
-			})
-			.catch(() => setNewSheetIsLoading(false));
-	};
-	//FIXME: The get started button does not work when live (most of the time)
-
 	const screenHeight = use100vh();
 
 	return (
@@ -83,13 +72,9 @@ const Home: React.FC = () => {
 							</Heading>
 							<Center>
 								<VStack spacing="break">
-									<Button
-										onClick={getNewSheet}
-										isLoading={newSheetIsLoading}
-										colorScheme="primary"
-									>
+									<LinkBox href="/new" as={Button} colorScheme="primary">
 										Get Started
-									</Button>
+									</LinkBox>
 									<ButtonLink href={infoPageUrl} variant="ghost" size="xs">
 										What is this?
 									</ButtonLink>
