a44eaa7e7c7bb9db2511e58092e4e42feaede986
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-30 12:19:24 +1100

    Component containing a link to the git repo

---
+++ components/ui/GitLink.tsx
@@ -0,0 +1,28 @@
+import { IconButton, IconButtonProps } from "@chakra-ui/button";
+import { LogoGithubIcon } from "chakra-ui-ionicons";
+import Link from "next/link";
+import { appGitLink } from "../../constants/meta";
+
+type GitLinkProps = Omit<IconButtonProps, "aria-label" | "icon" | "children">;
+
+/**
+ * An icon that works as a link to the Github repository
+ * page
+ *
+ * @param {object} props The props
+ * @returns {React.ReactElement} Icon button that links
+ * to the github repository
+ */
+const GitLink: React.FC<GitLinkProps> = (props) => (
+	<Link href={appGitLink}>
+		<IconButton
+			isRound
+			variant="ghost"
+			{...props}
+			aria-label="link to github repository"
+			icon={<LogoGithubIcon boxSize="icon" />}
+		/>
+	</Link>
+);
+
+export default GitLink;
