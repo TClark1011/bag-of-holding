7cfa6c057ee4ca1e3f47d4c8acf33dd832badc8f
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-01 09:22:44 +1100

    added label to 'carriedBy' item validation field

--- validation/itemValidation.ts
+++ validation/itemValidation.ts
@@ -7,7 +7,7 @@ const itemValidation = yup.object({
 	quantity: yup.number().min(1).required(),
 	weight: yup.number().min(0).required(),
 	value: yup.number().min(0).required(),
-	carriedBy: yup.string().required(),
+	carriedBy: yup.string().required().label("carried by"),
 	reference: yup.string(),
 });
 
