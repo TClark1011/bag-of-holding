bb360bf1e822f5cfd0721b4744167d4d2cd1a08e
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-31 12:17:04 +1100

    created hookstate object for new sheet page

---
+++ state/newSheetPageState.ts
@@ -0,0 +1,34 @@
+import { createState as createHookstate, useHookstate } from "@hookstate/core";
+
+export interface NewSheetPageState {
+	errorHasOccurred: boolean;
+}
+
+const newSheetPageState = createHookstate<NewSheetPageState>({
+	errorHasOccurred: false,
+});
+
+/**
+ * Hook to use the state values of the new sheet page
+ *
+ * @returns {object} The values/selectors/actions for
+ * the state
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const useNewSheetPageState = () => {
+	const state = useHookstate(newSheetPageState);
+	return {
+		//# SELECTORS
+		errorHasOccurred: state.errorHasOccurred.value,
+
+		//# ACTIONS
+		/**
+		 * Set 'error has occurred' to true
+		 */
+		turnOnError: () => {
+			state.errorHasOccurred.set(true);
+		},
+	};
+};
+
+export default newSheetPageState;
