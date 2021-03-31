09f142dcae3761069a702993a9bb752291693fbf
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-03-31 12:25:20 +1100

    finished 'new' page

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,10 +1,20 @@
-import { Center } from "@chakra-ui/layout";
+import { Box, Center } from "@chakra-ui/layout";
 import axios from "axios";
 import { useEffect } from "react";
 import { use100vh } from "react-div-100vh";
 import { newSheetApiRoute } from "../constants/urls";
 import { useRouter } from "next/router";
 import getSheetLink from "../utils/getSheetLink";
+import { H3 } from "../components/ui/Typography";
+import { Spinner } from "@chakra-ui/spinner";
+import { useNewSheetPageState } from "../state/newSheetPageState";
+import {
+	Alert,
+	AlertDescription,
+	AlertIcon,
+	AlertTitle,
+} from "@chakra-ui/alert";
+import { AlertCircleOutlineIcon } from "chakra-ui-ionicons";
 
 /**
  * Page to take user to when they want to make a new sheet.
@@ -16,19 +26,48 @@ import getSheetLink from "../utils/getSheetLink";
  */
 const New: React.FC = () => {
 	const router = useRouter();
+	const { errorHasOccurred, turnOnError } = useNewSheetPageState();
+
 	useEffect(() => {
 		console.log("Requesting new sheet");
 		axios
 			.get(newSheetApiRoute)
 			.then((res) => {
-				router.push(getSheetLink(res.data));
+				throw Error();
+				router.replace(getSheetLink(res.data));
 			})
-			.catch((err) => console.log(err))
+			.catch(() => {
+				turnOnError();
+			})
 			.finally(() => console.log("(new) Finished fetching "));
 	}, []);
 
 	const screenHeight = use100vh();
-	return <Center minHeight={screenHeight}>Loading...</Center>;
+	return (
+		<Center minHeight={screenHeight} padding="break">
+			{errorHasOccurred ? (
+				<Alert
+					status="error"
+					colorScheme="error"
+					variant="left-accent"
+					width="max-content"
+				>
+					<AlertIcon />
+					<AlertTitle>An error has occurred</AlertTitle>
+					<AlertDescription>Please try again later</AlertDescription>
+				</Alert>
+			) : (
+				<Box>
+					<H3 textAlign="center" marginBottom="break">
+						Creating a new sheet
+					</H3>
+					<Center>
+						<Spinner />
+					</Center>
+				</Box>
+			)}
+		</Center>
+	);
 };
 
 export default New;
