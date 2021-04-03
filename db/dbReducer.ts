f1d970dd616aa06bd1533a22b923435f6bd4d519
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-03 15:35:35 +1100

    removed comments

--- db/dbReducer.ts
+++ db/dbReducer.ts
@@ -14,28 +14,36 @@ const dbReducer = async (
 	sheetId: string,
 	action: InventorySheetStateAction
 ): Promise<void> => {
+	const incrementVersion = { $inc: { __v: 1 } };
 	if (action.type === "item_add") {
-		console.log("(dbReducer) dbReducer received item_add action!");
-
+		//# Add An Item
 		SheetModel.findByIdAndUpdate(sheetId, {
 			$push: { items: action.data },
+			...incrementVersion,
 		}).exec();
 	} else if (action.type === "item_remove") {
+		//# Remove An Item
 		SheetModel.findByIdAndUpdate(sheetId, {
 			$pull: { items: { _id: action.data } } as any,
+			...incrementVersion,
 		}).exec();
 	} else if (action.type === "item_update") {
+		//# Update an item
 		const update: Record<string, string | number> = {};
 		Object.entries(action.data).forEach(([key, value]) => {
 			update["items.$." + key] = value;
 		});
+		//? Create the object that will be used to update the sheet
+
 		SheetModel.updateMany(
 			{ _id: sheetId, "items._id": action.data._id } as any,
-			{ $set: update } as any
+			{ $set: update, ...incrementVersion } as any
 		).exec();
 	} else if (action.type === "sheet_metadataUpdate") {
+		//# Update sheet metadata
 		SheetModel.findByIdAndUpdate(sheetId, {
 			$set: { name: action.data.name, members: action.data.members },
+			...incrementVersion,
 		}).exec();
 	}
 };
