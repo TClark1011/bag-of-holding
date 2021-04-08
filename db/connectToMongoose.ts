bd193b1fd9c3ceac61608288b8748a3a3649fc0f
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-08 11:28:58 +1000

    refactored `connectToMongoose`

--- db/connectToMongoose.ts
+++ db/connectToMongoose.ts
@@ -4,14 +4,11 @@ import { MONGO_URL } from "../config/env";
 /**
  * Establish a connection to MongoDB via mongoose
  */
-const connectToMongoose = (): void => {
-	mongoose
-		.connect(MONGO_URL, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			useFindAndModify: false,
-		})
-		.then(() => console.log("connected to mongoose"));
-};
+const connectToMongoose = async (): Promise<typeof mongoose> =>
+	mongoose.connect(MONGO_URL, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useFindAndModify: false,
+	});
 
 export default connectToMongoose;
