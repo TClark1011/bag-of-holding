54571a38b1a5e04687403078b03f250e04937592
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-08 10:43:39 +1000

    env fetching no longer uses 'throw-env'

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,21 @@
-import throwEnv from "throw-env";
+import { config } from "dotenv-flow";
 
-export const MONGO_URL = throwEnv("MONGO_URL");
+config();
+
+/**
+ * Fetch environment variables, throwing an error if they are
+ * not present
+ *
+ * @param {string} key The key used to access the environment
+ * data.
+ * @returns {string} The raw data pulled from the environment
+ */
+const getEnvOrThrow = (key: string): string => {
+	const data = process.env[key];
+	if (!data) {
+		throw Error(`Required environment variable ${key} was not found`);
+	}
+	return data;
+};
+
+export const MONGO_URL = getEnvOrThrow("MONGO_URL");
