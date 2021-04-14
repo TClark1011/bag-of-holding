1a38c75f9b3c8f5a857f10c431ece3e9e7ec2d09
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-14 15:28:36 +1000

    initial 'View' template created

---
+++ components/templates/View.tsx
@@ -0,0 +1,15 @@
+export interface ViewProps {
+	showTopBar: boolean;
+	showHomePageLink?: boolean;
+}
+
+/**
+ * @param root0
+ * @param root0.showTopBar
+ * @param root0.showHomePageLink
+ */
+const View: React.FC<ViewProps> = ({ showTopBar, showHomePageLink = true }) => {
+	return null;
+};
+
+export default View;
