393181389cba81ec9170f7f4bc2568aff028dd4b
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-15 13:42:01 +1000

    content for info page

--- pages/info.tsx
+++ pages/info.tsx
@@ -1,10 +1,9 @@
-import { Box, Center, Flex, VStack } from "@chakra-ui/layout";
-import { chakra } from "@chakra-ui/system";
+import { Box, VStack } from "@chakra-ui/layout";
 import View from "../components/templates/View";
 import { H3, Paragraph, SectionTitle } from "../components/ui/Typography";
 import { infoPageUrl } from "../constants/urls";
 
-const QuestionTitle = chakra(H3, { baseStyle: { marginBottom: 4 } });
+export const 
 
 /**
  * @param root0
@@ -35,8 +34,22 @@ const InfoPage: React.FC = () => {
 					<InfoPageQuestion title="What is Bag of Holding?">
 						<Paragraph>
 							Bag of Holding is a web app that {"let's"} tabletop RPG players
-							track their {"group's"} inventory with ease by allowing players to
-							pool their individual inventories into a central location.
+							track their {"group's"} inventory with ease.
+						</Paragraph>
+						<Paragraph>
+							The standard approach to RPG inventory is for each play to track
+							their own individual inventory, with a single player often elected
+							to keep track of items that are shared amongst the party.
+						</Paragraph>
+						<Paragraph>
+							Now groups can break away from this approach, with a single
+							central location to track {"everybody's"} inventory in a single
+							place, collaboration becomes easy.
+						</Paragraph>
+						<Paragraph>
+							With features like searching, sorting and filtering items by who is
+							carrying them, players can still track their own inventory quickly
+							and conveniently.
 						</Paragraph>
 					</InfoPageQuestion>
 					<InfoPageQuestion title="How Do I Save My Sheet?"></InfoPageQuestion>
