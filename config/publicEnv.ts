f24723f9dfe14d76badcf6446d3c4c95c9072b14
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-16 13:06:08 +1000

    added env variable to check if app is building
    Specifically if it is building to be deployed to production.

--- config/publicEnv.ts
+++ config/publicEnv.ts
@@ -6,3 +6,10 @@ export const REFETCH_INTERVAL = parseInt(
 export const inProduction = process.env.NODE_ENV === "production";
 export const inDevelopment = process.env.NODE_ENV === "development";
 export const inTesting = process.env.NODE_ENV === "test";
+
+export const onProdBranch =
+	process.env.ON_PROD_BRANCH && JSON.parse(process.env.ON_PROD_BRANCH) === true;
+//? If the application is running on the production branch
+
+export const isBuildingForProd = onProdBranch && inProduction;
+//? If the application is being built to be deployed to production
