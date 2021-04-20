b8a8d98d73f7f37ec86fcb39545623d2189764e2
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-20 11:20:34 +1000

    created file with analytics functions

---
+++ utils/analytics.ts
@@ -0,0 +1,19 @@
+import GoogleAnalytics from "react-ga";
+
+/**
+ * Log a page view in Google Analytics
+ */
+export const logPageView = (): void => {
+	GoogleAnalytics.set({ page: window.location.pathname });
+	GoogleAnalytics.pageview(window.location.pathname);
+};
+
+/**
+ * Log an event in Google Analytics
+ *
+ * @param {string} category The category of the event
+ * @param {string} action The action of the event
+ */
+export const logEvent = (category: string, action: string): void => {
+	GoogleAnalytics.event({ category, action });
+};
