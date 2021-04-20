eceb6a2c4bfb8e0c35089698ead1f30b3c5a17f4
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-20 11:22:34 +1000

    export google analytics id as public env var

--- config/publicEnv.ts
+++ config/publicEnv.ts
@@ -14,3 +14,5 @@ export const onProdBranch =
 
 export const isBuildingForProd = onProdBranch && inProduction;
 //? If the application is being built to be deployed to production
+
+export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
