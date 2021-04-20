1eb236ebd18de8868c6d066e547624bf7e5169c4
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-20 11:22:16 +1000

    created global state store

---
+++ state/globalState.ts
@@ -0,0 +1,35 @@
+import GoogleAnalytics from "react-ga";
+import { createState as createHookstate, useHookstate } from "@hookstate/core";
+import { GOOGLE_ANALYTICS_ID } from "../config/publicEnv";
+
+export interface GlobalStoreFields {
+	googleAnalyticsInitialised: boolean;
+}
+
+export const globalState = createHookstate<GlobalStoreFields>({
+	googleAnalyticsInitialised: false,
+});
+
+/**
+ *
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const useGlobalState = () => {
+	const state = useHookstate(globalState);
+
+	return {
+		//# VALUES/SELECTORS
+		googleAnalyticsInitialised: state.googleAnalyticsInitialised.value,
+
+		//# ACTIONS
+		/**
+		 *
+		 */
+		initialiseGoogleAnalytics: () => {
+			if (!state.googleAnalyticsInitialised.value) {
+				GoogleAnalytics.initialize(GOOGLE_ANALYTICS_ID);
+				state.googleAnalyticsInitialised.set(true);
+			}
+		},
+	};
+};
