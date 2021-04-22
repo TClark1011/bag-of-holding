8b1ea730f054265d8206c8f9a417e49d804fc258
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-23 09:24:23 +1000

    test(workspace): :wrench: implemented jest tests
    tests can now be easily executed with a single command, with the output of all tests appearing in a single combined output

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 	testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 	testTimeout: 10000,
+	projects: [
+		{
+			displayName: "frontend",
+			rootDir: "tests/frontend",
+			setupFilesAfterEnv: ["../../jest.setup.ts"],
+		},
+		{
+			displayName: "backend",
+			rootDir: "tests/backend",
+			testEnvironment: "node",
+			setupFilesAfterEnv: ["../../jest.setup.ts"],
+		},
+	],
 };
