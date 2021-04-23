62d4769bfb85fceb3f4ba83c0623739b1649391a
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-23 13:39:18 +1000

    fix: :bug: HOTFIX: added timestamps to sheet schema

--- db/SheetModel.ts
+++ db/SheetModel.ts
@@ -1,23 +1,26 @@
 import mongoose, { Model, Document } from "mongoose";
 import InventorySheetFields from "../types/InventorySheetFields";
 
-const SheetSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-	members: { type: [String] },
-	items: [
-		{
-			_id: { type: String, required: true },
-			name: { type: String, required: true },
-			description: { type: String },
-			category: { type: String, default: "None" },
-			value: { type: Number },
-			weight: { type: Number },
-			quantity: { type: Number },
-			carriedBy: { type: String, default: "Nobody" },
-			reference: { type: String },
-		},
-	],
-});
+const SheetSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		members: { type: [String] },
+		items: [
+			{
+				_id: { type: String, required: true },
+				name: { type: String, required: true },
+				description: { type: String },
+				category: { type: String, default: "None" },
+				value: { type: Number },
+				weight: { type: Number },
+				quantity: { type: Number },
+				carriedBy: { type: String, default: "Nobody" },
+				reference: { type: String },
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 const SheetModel =
 	(mongoose.models.sheet as Model<Document<InventorySheetFields>>) ||
