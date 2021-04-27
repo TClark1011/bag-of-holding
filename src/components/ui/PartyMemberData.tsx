8e42f5b5985f8737af5cd0daac6074d9669c8561
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-04-28 09:14:20 +1000

    chore(frontend): :recycle: created component for displaying data about a party member

---
+++ src/components/ui/PartyMemberData.tsx
@@ -0,0 +1,37 @@
+import { Text, TextProps } from "@chakra-ui/layout";
+import React from "react";
+import InventoryMemberFields from "../../types/InventoryMemberFields";
+import { OmitId } from "../../types/UtilityTypes";
+import { useInventoryState } from "../contexts/InventoryStateContext";
+
+export interface PartyMemberDataProps extends Omit<TextProps, "children"> {
+	memberId: string;
+	property: keyof OmitId<InventoryMemberFields>;
+}
+
+/**
+ * Display a single property of a member with the _id
+ * that matches a provided id string.
+ *
+ * @param {object} props The props
+ * @param {string} props.memberId The id of the member to get the
+ * data of
+ * @param {string} props.property The data property to display
+ * @returns {React.ReactNode} The fetched data inside a 'Text'
+ * component. If no matching member is found, null is returned
+ */
+const PartyMemberData: React.FC<PartyMemberDataProps> = ({
+	memberId,
+	property,
+	...props
+}) => {
+	const { members } = useInventoryState();
+
+	const selectedMember = members.find((item) => item._id === memberId);
+
+	return selectedMember ? (
+		<Text {...props}>{selectedMember[property]}</Text>
+	) : null;
+};
+
+export default PartyMemberData;
