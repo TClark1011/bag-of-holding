5232a120ef187e24577112341dd058ced750b88d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-05-13 08:51:15 +1000

    chore(fullstack): :fire: removed unused generation function

--- tests/fixtures/itemFixtures.ts
+++ tests/fixtures/itemFixtures.ts
@@ -1,24 +1,4 @@
-import faker from "faker";
 import InventoryItemFields from "../../src/types/InventoryItemFields";
-import { generateRandomInventoryItem } from "../../src/utils/createInventoryItem";
-import { averageMembersFixture } from "./membersFixtures";
-
-/**
- * Generate an array of random inventory items
- *
- * @param {number} [amount]  The number of items to generate. Defaults to a
- * random value between 1 and 50.
- * @returns {InventoryItemFields[]} An array of randomly generated inventory items
- */
-export const getRandomInventoryItems = (
-	amount: number = faker.random.number({ min: 1, max: 50 })
-): InventoryItemFields[] => {
-	const result: InventoryItemFields[] = [];
-	for (let i = 0; i < amount; i++) {
-		result.push(generateRandomInventoryItem({}, averageMembersFixture));
-	}
-	return result;
-};
 
 export const longswordFixture: InventoryItemFields = {
 	_id: "longsword _id",
