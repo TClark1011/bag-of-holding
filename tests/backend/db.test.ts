a68f5f441cc3e6af8fff888b5a37ad6a0b7bba6e
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-05-13 10:09:13 +1000

    test(backend): :white_check_mark: completed dbReducer metadata tests

--- tests/backend/db.test.ts
+++ tests/backend/db.test.ts
@@ -6,14 +6,20 @@ import InventorySheetFields from "../../src/types/InventorySheetFields";
 import mongoose from "mongoose";
 import { MockMongoose } from "mock-mongoose";
 import {
-	AddItemAction,
 	InventorySheetPartialUpdateAction,
 	UpdateSheetMetaDataAction,
 } from "../../src/types/InventorySheetState";
 import generateMember from "../../src/generators/generateMember";
 import InventoryMemberFields from "../../src/types/InventoryMemberFields";
-import { memberIsCarrying } from "../../src/utils/inventoryItemUtils";
 import { OmitId } from "../../src/types/UtilityTypes";
+import {
+	healthPotionFixture,
+	longswordFixture,
+} from "../fixtures/itemFixtures";
+import { merge } from "merge-anything";
+import { memberIsCarrying } from "../../src/utils/inventoryItemUtils";
+import getCarriedItems from "../../src/utils/getCarriedItems";
+import tweakString from "../utils/tweakString";
 
 let sheetId = "";
 //? Variable to store the id of the sheet we create for testing
@@ -59,9 +65,10 @@ afterAll(async () => {
  * - initialState: The state of sheet as it was created
  * - update: A function to dispatch actions on that sheet
  * and then return the updated state
- * @param {object} [initialState] The state used to initialise
- * the new sheet. Defaults to a sheet with a basic name and 
- * no members or items.
+ * @param {object} [initialStateTweaks={}] An object which
+ * is merged with a basic sheet inventory state to derive
+ * derive the starting state of the sheet. The basic initial
+ * sheet has the name "name" and no members or items.
  */
 const testDbReducer = async (
 	testFn: (
@@ -70,13 +77,18 @@ const testDbReducer = async (
 			action: InventorySheetPartialUpdateAction
 		) => Promise<InventorySheetFields>
 	) => Promise<void>,
-	initialState: OmitId<InventorySheetFields> = {
-		name: "name",
-		members: [],
-		items: [],
-	}
+	initialStateTweaks: Partial<OmitId<InventorySheetFields>> = {}
 ): Promise<void> => {
-	const newSheet = await new SheetModel(initialState).save();
+	const newSheet = await new SheetModel(
+		merge(
+			{
+				name: "name",
+				members: [],
+				items: [],
+			},
+			initialStateTweaks
+		)
+	).save();
 
 	/**
 	 * Update the sheet via an action and return the
@@ -177,7 +189,27 @@ describe("Metadata Member updates", () => {
 		generateMember("1"),
 	];
 
-	test("Add members", () =>
+	/**
+	 * @param member
+	 * @param member2
+	 */
+	const getTestItems = (member: InventoryMemberFields, member2 = member) => [
+		{ ...longswordFixture, carriedBy: member._id },
+		{ ...healthPotionFixture, carriedBy: member2._id },
+	];
+
+	test("Add member", () =>
+		testDbReducer(async (originalState, update) => {
+			const updated = await update({
+				type: "sheet_metadataUpdate",
+				data: {
+					name: originalState.name,
+					members: { add: [testMembers[0]], remove: [], update: [] },
+				},
+			});
+			expect(updated.members.length).toEqual(1);
+		}));
+	test("Add multiple members", () =>
 		testDbReducer(async (originalState, update) => {
 			const updated = await update({
 				type: "sheet_metadataUpdate",
@@ -186,144 +218,285 @@ describe("Metadata Member updates", () => {
 					members: { add: testMembers, remove: [], update: [] },
 				},
 			});
-			expect(updated.members.length).toEqual(2);
+			expect(updated.members.length).toEqual(testMembers.length);
 		}));
 
-	test("Remove member - Delete Items", async () => {
-		const originalState = await getSheet();
+	test("Remove member - Remove item", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				//FIXME: This test always passes even with `expect(false).toBeTruthy();`
+				expect(originalState.items.length).toEqual(1);
 
-		const removeMember = testMembers[0];
-
-		await dbReducer(sheetId, {
-			type: "item_add",
-			data: {
-				name: "item",
-				carriedBy: removeMember._id,
-			},
-		});
-		//? Adding an item that should then be removed when we delete the member
-
-		expect((await getSheet()).items.length).toEqual(1);
-		//? We check that the item was added correctly
-
-		await dispatchMetaDataUpdate({
-			name: originalState.name,
-			members: {
-				add: [],
-				remove: [
-					{
-						...removeMember,
-						deleteMethod: {
-							mode: "remove",
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [
+								{
+									...testMembers[0],
+									deleteMethod: {
+										mode: "remove",
+									},
+								},
+							],
+							update: [],
 						},
 					},
-				],
-				update: [],
+				});
+				expect(updated.items.length).toEqual(0);
 			},
-		});
-		//? Dispatch the update to remove the member
+			{
+				members: [testMembers[0]],
+				items: [getTestItems(testMembers[0])[0]],
+			}
+		));
 
-		const updatedState = await getSheet();
+	test("Remove member - Remove multiple items", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				expect(originalState.items.length).toEqual(2);
 
-		expect(updatedState.members.length).toEqual(0);
-		expect(updatedState.items.length).toEqual(0);
-	});
-
-	test("Add multiple members in one update", async () => {
-		const originalState = await getSheet();
-
-		await dispatchMetaDataUpdate({
-			name: originalState.name,
-			members: {
-				add: testMembers,
-				remove: [],
-				update: [],
-			},
-		});
-
-		const updatedState = await getSheet();
-
-		expect(updatedState.members.length).toEqual(2);
-	});
-
-	test("Remove member - Move Items", async () => {
-		const originalState = await getSheet();
-
-		const [fromMember, toMember] = testMembers;
-
-		await dispatchMetaDataUpdate({
-			name: originalState.name,
-			members: {
-				add: [fromMember],
-				remove: [],
-				update: [],
-			},
-		});
-
-		const numberOfItemsToAdd = 3;
-		for (let i = 0; i < numberOfItemsToAdd; i++) {
-			await dbReducer(sheetId, {
-				type: "item_add",
-				data: {
-					name: "item" + i,
-					carriedBy: fromMember._id,
-				},
-			});
-		}
-
-		expect((await getSheet()).items.length).toEqual(numberOfItemsToAdd);
-
-		await dispatchMetaDataUpdate({
-			name: originalState.name,
-			members: {
-				add: [],
-				remove: [
-					{
-						...fromMember,
-						deleteMethod: {
-							mode: "move",
-							to: toMember._id,
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [
+								{
+									...testMembers[0],
+									deleteMethod: {
+										mode: "remove",
+									},
+								},
+							],
+							update: [],
 						},
 					},
-				],
-				update: [],
+				});
+				expect(updated.items.length).toEqual(0);
 			},
-		});
+			{
+				members: [testMembers[0]],
+				items: getTestItems(testMembers[0]),
+			}
+		));
+	test("Remove member - Move item to another member", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				expect(originalState.items.length).toEqual(2);
 
-		const multiUpdatedState = await getSheet();
-
-		expect(
-			multiUpdatedState.items.filter((item) => memberIsCarrying(toMember, item))
-				.length
-		).toEqual(numberOfItemsToAdd);
-	});
-
-	test("Remove Member - Set To Nobody", async () => {
-		const originalState = await getSheet();
-		await dispatchMetaDataUpdate({
-			name: originalState.name,
-			members: {
-				add: [],
-				remove: [
-					{
-						...testMembers[1],
-						deleteMethod: {
-							mode: "setToNobody",
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [
+								{
+									...testMembers[0],
+									deleteMethod: {
+										mode: "move",
+										to: testMembers[1]._id,
+									},
+								},
+							],
+							update: [],
 						},
 					},
-				],
-				update: [],
+				});
+				expect(updated.items.length).toEqual(2);
+				expect(updated.items).toEqual(
+					getCarriedItems(updated.items, testMembers[1])
+				);
 			},
-		});
+			{
+				members: [testMembers[0], testMembers[1]],
+				items: getTestItems(testMembers[0], testMembers[1]),
+			}
+		));
+	test("Remove member - Move multiple items to another member", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				expect(originalState.items.length).toEqual(3);
+				expect(
+					getCarriedItems(originalState.items, testMembers[0]).length
+				).toEqual(1);
+				expect(
+					getCarriedItems(originalState.items, testMembers[1]).length
+				).toEqual(2);
 
-		const updatedState = await getSheet();
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [
+								{
+									...testMembers[1],
+									deleteMethod: {
+										mode: "move",
+										to: testMembers[0]._id,
+									},
+								},
+							],
+							update: [],
+						},
+					},
+				});
+				expect(updated.items.length).toEqual(3);
+				expect(updated.items).toEqual(
+					getCarriedItems(updated.items, testMembers[0])
+				);
+			},
+			{
+				members: [testMembers[0], testMembers[1]],
+				items: [
+					{
+						_id: "1",
+						name: " ",
+						quantity: 1,
+						weight: 1,
+						carriedBy: testMembers[0]._id,
+					},
+					{
+						_id: "2",
+						name: " ",
+						quantity: 1,
+						weight: 1,
+						carriedBy: testMembers[1]._id,
+					},
+					{
+						_id: "3",
+						name: " ",
+						quantity: 1,
+						weight: 1,
+						carriedBy: testMembers[1]._id,
+					},
+				],
+			}
+		));
 
-		expect(
-			updatedState.items.filter((item) => item.carriedBy === "Nobody").length
-		).toEqual(
-			originalState.items.filter((item) =>
-				memberIsCarrying(testMembers[1], item)
-			).length
-		);
-	});
+	test("Remove member - Set to nobody", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				expect(originalState.items.length).toEqual(1);
+
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [
+								{
+									...testMembers[0],
+									deleteMethod: {
+										mode: "setToNobody",
+									},
+								},
+							],
+							update: [],
+						},
+					},
+				});
+				expect(updated.items.length).toEqual(1);
+				expect(updated.items).toEqual(getCarriedItems(updated.items, "Nobody"));
+			},
+			{
+				members: [testMembers[0]],
+				items: [getTestItems(testMembers[0])[0]],
+			}
+		));
+	test("Remove member - Set multiple to nobody", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				expect(originalState.items.length).toEqual(2);
+
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [
+								{
+									...testMembers[0],
+									deleteMethod: {
+										mode: "setToNobody",
+									},
+								},
+							],
+							update: [],
+						},
+					},
+				});
+				expect(updated.items.length).toEqual(originalState.items.length);
+				expect(updated.items).toEqual(getCarriedItems(updated.items, "Nobody"));
+			},
+			{
+				members: [testMembers[0]],
+				items: getTestItems(testMembers[0]),
+			}
+		));
+	test("Update member", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [],
+							update: [
+								{
+									...testMembers[0],
+									name: tweakString(testMembers[0].name),
+								},
+							],
+						},
+					},
+				});
+				expect(updated.members).not.toEqual(originalState.members);
+				expect(updated.members.length).toEqual(originalState.members.length);
+				expect(updated.members[0].name).toEqual(
+					tweakString(testMembers[0].name)
+				);
+			},
+			{
+				members: [testMembers[0]],
+			}
+		));
+	test("Update multiple members", () =>
+		testDbReducer(
+			async (originalState, update) => {
+				const updated = await update({
+					type: "sheet_metadataUpdate",
+					data: {
+						name: originalState.name,
+						members: {
+							add: [],
+							remove: [],
+							update: testMembers.map(({ name, ...mem }) => ({
+								...mem,
+								name: tweakString(name),
+							})),
+						},
+					},
+				});
+
+				expect(updated.members).not.toEqual(originalState.members);
+				expect(updated.members.length).toEqual(originalState.members.length);
+				expect(updated.members.map((m) => m.name)).toEqual(
+					originalState.members.map((m) => tweakString(m.name))
+				);
+			},
+			{
+				members: testMembers,
+			}
+		));
 });
