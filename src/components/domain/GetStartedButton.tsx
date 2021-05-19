170d4b4bd999ff822514a7cc4e9264640c6a60da
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-05-19 12:47:27 +1000

    feat(frontend): :sparkles: GetStartedButton returns maintenance message when app is in maintenance

--- src/components/domain/GetStartedButton.tsx
+++ src/components/domain/GetStartedButton.tsx
@@ -1,5 +1,7 @@
 import { ButtonProps } from "@chakra-ui/button";
+import { MAINTENANCE_MODE } from "../../config/publicEnv";
 import ButtonLink from "../ui/ButtonLink";
+import { H3 } from "../ui/Typography";
 
 /**
  * Button to create a new sheet.
@@ -11,7 +13,9 @@ import ButtonLink from "../ui/ButtonLink";
  * @returns {React.ReactElement} The rendered button
  */
 const GetStartedButton: React.FC<ButtonProps> = (props) => {
-	return (
+	return MAINTENANCE_MODE ? (
+		<H3>Currently Undergoing Maintenance</H3>
+	) : (
 		<ButtonLink href="/new" colorScheme="primary" {...props}>
 			Get Started
 		</ButtonLink>
