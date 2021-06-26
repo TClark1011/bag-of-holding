0815f14dfd4bf427cf0a5f502d8570598f023cb6
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-06-26 14:45:19 +1000

    fix(workspace): :fire: removed prod blocker

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,7 +5,6 @@ import ButtonLink from "../components/ui/ButtonLink";
 import { Paragraph, SectionTitle } from "../components/ui/Typography";
 import { appName, contactEmailAddress } from "../constants/branding";
 import { contactPageUrl } from "../constants/urls";
-import blockProdBuild from "../utils/blockProdBuild";
 import createPageTitle from "../utils/createPageTitle";
 
 export const contactPageTitle = "Contact";
@@ -13,11 +12,9 @@ export const contactPageTitle = "Contact";
 /**
  * The page containing contact information
  *
- * @returns {React.ReactElement} The contact page
+ * @returns {React.ReactElement} The contact pageobsiob
  */
 const ContactPage: React.FC = () => {
-	blockProdBuild(`Confirm that contact email '${contactEmailAddress}' works`);
-
 	return (
 		<View
 			title={createPageTitle("Contact")}
