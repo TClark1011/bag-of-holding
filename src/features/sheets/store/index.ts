dcfcdec1f492b7bb8709772308663e55be0668c8
Thomas Clark <thomas.clark@bigtincan.com>
2023-06-01 12:43:20 +1000

    refactor(workspace): 💡 removed unused axios code and dependency

--- src/features/sheets/store/index.ts
+++ src/features/sheets/store/index.ts
@@ -1,5 +1,3 @@
-export { default as inventoryReducer } from "./inventoryReducer";
-export { default as useInventoryReducer } from "./useInventoryReducer";
 export { default as useInventoryStore } from "./useInventoryStore";
 
 export * from "./useInventoryStore";
